2af1f72bf5cfaa039d5d36ea3ad014d6b656baf3
Davide Bertuzzi <bertuzzi@digiunit.it>
2023-03-21 22:51:37 +0100

    Commit: Twitter v1.0.0

--- controllers/twitterControllers.js
+++ controllers/twitterControllers.js
@@ -1,5 +1,9 @@
+const twitter = require("../utilities/twitter");
+
 const index = async (req, res, next) => {
   try {
+    await twitter.setRules();
+    await twitter.stream();
     const twitters = [{ name: "Twit 1" }, { name: "Twit 2" }];
     res.status(200).json(twitters);
   } catch (err) {
