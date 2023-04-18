57caf3f011f0e6147670d70af93664e498428066
Davide Bertuzzi <bertuzzi@digiunit.it>
2023-04-19 00:04:16 +0200

    Commit: auth route improvements

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { body } = require("express-validator");
+const { param, body } = require("express-validator");
 const authControllers = require("../controllers/authControllers");
 const router = express.Router();
 
 router.get("/", authControllers.index);
+router.get("/:uid", param("uid").notEmpty(), authControllers.indexId);
 router.post(
   "/",
   body("email").isString().isEmail().notEmpty().escape(),
@@ -16,7 +17,7 @@ router.put(
   "/",
   body("uid").isString().notEmpty().escape(),
   body("email").isString().isEmail().notEmpty().escape(),
-  body("password").isString().notEmpty().isLength({ min: 6 }).escape(),
+  // body("password").isString().notEmpty().isLength({ min: 6 }).escape(),
   body("displayName").isString().notEmpty().escape(),
   body("userRoles").isArray().notEmpty().optional({ nullable: true }),
   authControllers.update
