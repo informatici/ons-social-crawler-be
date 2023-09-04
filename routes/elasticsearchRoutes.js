4ec61826d7a5a897c55d3c5fa79b18ed64535281
Davide Bertuzzi <bertuzzi@digiunit.it>
2023-09-04 11:02:50 +0200

    Commit: API query

--- routes/elasticsearchRoutes.js
+++ routes/elasticsearchRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { param, query } = require("express-validator");
 const { isAuthorized } = require("../utilities/firebase.js");
 const elasticsearchControllers = require("../controllers/elasticsearchControllers.js");
 const router = express.Router();
@@ -6,5 +7,13 @@ const router = express.Router();
 router.get("/info", isAuthorized(["Admin"]), elasticsearchControllers.info);
 router.get("/config", isAuthorized(["Admin"]), elasticsearchControllers.config);
 router.get("/clean", isAuthorized(["Admin"]), elasticsearchControllers.clean);
+
+router.get(
+  "/query/:social",
+  param("social").notEmpty(),
+  query("size").notEmpty(),
+  query("page").notEmpty(),
+  elasticsearchControllers.query
+);
 
 module.exports = router;
