c9ab8e14adca42bb45f927146d727097237c359d
Davide Bertuzzi <bertuzzi@digiunit.it>
2023-10-03 23:36:20 +0200

    Commit: little improvements

--- utilities/twitter.js
+++ utilities/twitter.js
@@ -5,6 +5,8 @@ const streamStatus = require("../utilities/streamStatus");
 
 let countTweets = 0;
 let twitterLength = 0;
+let lastId = "";
+let count = 0;
 const instance = axios.create({
   baseURL: "https://api.twitter.com/2/",
   // timeout: 1000,
@@ -26,40 +28,46 @@ const get = (slug) => {
 };
 
 const saveTweets = async (pageToken = "") => {
-  const lastId = await elasticsearch.getLastTwitId();
   // console.log("lastId", lastId);
   let responseTweets = "";
   if (!lastId) {
     responseTweets = await get(
-      `tweets/search/recent?expansions=author_id,referenced_tweets.id&sort_order=relevancy&tweet.fields=lang,created_at,note_tweet&query=(sport OR calcio) lang:it -is:retweet (is:reply OR is:quote)`
+      `tweets/search/recent?expansions=author_id,referenced_tweets.id&sort_order=relevancy&tweet.fields=lang,created_at,note_tweet&query=(sport OR calcio) lang:it -is:retweet -is:quote`
     );
   } else if (pageToken) {
     responseTweets = await get(
-      `tweets/search/recent?expansions=author_id,referenced_tweets.id&sort_order=relevancy&tweet.fields=lang,created_at,note_tweet&since_id=${lastId}&query=(sport OR calcio) lang:it -is:retweet (is:reply OR is:quote)&next_token=${pageToken})`
+      `tweets/search/recent?expansions=author_id,referenced_tweets.id&sort_order=relevancy&tweet.fields=lang,created_at,note_tweet&since_id=${lastId}&query=(sport OR calcio) lang:it -is:retweet -is:quote&pagination_token=${pageToken}`
     );
   } else {
-    //Se lastId troppo vecchio
     try {
       responseTweets = await get(
-        `tweets/search/recent?expansions=author_id,referenced_tweets.id&sort_order=relevancy&tweet.fields=lang,created_at,note_tweet&since_id=${lastId}&query=(sport OR calcio) lang:it -is:retweet (is:reply OR is:quote)`
+        `tweets/search/recent?expansions=author_id,referenced_tweets.id&sort_order=relevancy&tweet.fields=lang,created_at,note_tweet&since_id=${lastId}&query=(sport OR calcio) lang:it -is:retweet -is:quote`
       );
     } catch {
+      //Se lastId troppo vecchio
       responseTweets = await get(
-        `tweets/search/recent?expansions=author_id,referenced_tweets.id&sort_order=relevancy&tweet.fields=lang,created_at,note_tweet&query=(sport OR calcio) lang:it -is:retweet (is:reply OR is:quote)`
+        `tweets/search/recent?expansions=author_id,referenced_tweets.id&sort_order=relevancy&tweet.fields=lang,created_at,note_tweet&query=(sport OR calcio) lang:it -is:retweet -is:quote`
       );
     }
   }
 
+  // &since_id=${lastId}
+  // &since_id=${lastId}
+  // query=(sport OR calcio) lang:it -is:retweet -is:quote
+
   const tweets = responseTweets?.data?.data || [];
   const nextPageToken = responseTweets?.data?.meta?.next_token || "";
   // console.log("tweets", responseTweets.data);
-  // console.log("nextPageToken", nextPageToken);
+  console.log("nextPageToken", nextPageToken);
 
   for (t of tweets) {
+    count++;
+    console.log(count);
     const lang = t.lang || "";
 
     if (lang === "it") {
       countTweets = await elasticsearch.indexTwit(t, countTweets);
+      console.log("countTweets", countTweets);
     }
 
     if (countTweets >= twitterLength) {
@@ -77,6 +85,8 @@ exports.getTweets = async () => {
   if (res.twitterLength > 0) {
     twitterLength = res.twitterLength;
     countTweets = 0;
+    lastId = await elasticsearch.getLastTwitId();
+    count = 0;
     await saveTweets();
   }
 };
