098ed5b18f2bcf16510ec40d01dc6a517d3ac15b
Davide Bertuzzi <bertuzzi@digiunit.it>
2023-10-06 15:44:45 +0200

    Commit: little improvements

--- utilities/firebase.js
+++ utilities/firebase.js
@@ -1,22 +1,22 @@
 var admin = require("firebase-admin");
 
 // var serviceAccount = require("../firebase/dev-ons-firebase-adminsdk-rcogp-7923c4b7baaa.json");
-var serviceAccount = require("../firebase/ones2-aid012318-02-04-firebase-adminsdk-hmhmj-99f3b89029.json");
+// var serviceAccount = require("../firebase/ones2-aid012318-02-04-firebase-adminsdk-hmhmj-99f3b89029.json");
 
-// const serviceAccount = {
-//   type: "service_account",
-//   project_id: "dev-ons",
-//   private_key_id: "7923c4b7ba087475d61b6d7cfbfe2713c4927b05",
-//   private_key:
-//     "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDcZ9cyZ3qU/lqj\nBt8QkjI7pyK5Ucl76If5wBMaY+ljD480IVDDILM2EUewmMRASxjv6CvyOSNL80HP\nbXvuRfW5r+yORqEiTS6TRgsoOFN9L0qj9OgVWI0ZrklaYYwZD5+vRAwxvJ0uyK/j\noOwJMRoYozzUY4GXj1pLqIuOUZvpYs41WFucWg5UUSbeubBmvR0mLBcD0Utj6i1S\ne8iDHa3SnyUpayIKeyR8Q6jF+BchSvBISPPgtuJE9P6vGylwqt66AD/Nm+6UVvFO\nvANxwNZjnJdRo5UuL+znYF3qI7ftz9LlWTUd2kOcqEpBB30uGv5OGw1zcwVB/i7P\nRTkg2k+LAgMBAAECggEAKXFxGm4ws4FcQ5lQKkqDHYpg0h3Z1+2yW7jNHsI4qULM\nt42oJPiqGfmH1GH1TO2SA9JRKRurUmWsMzev/O60tLSqs1n+hHek0VZzVJ26vSg+\nTbQIS7M53OFTs6Xx9OhmK0uNS3De0WQ1BoVEvLm54XYYQwtu27O908oea6VWvrxX\nCvquwvqeiLHFMMtr5VE82+WLCGc2nbZsOvIrF6M/2VkENxM3Hcg7vxgLv082P8K5\n8iDHMyBV3Y+ZTNVVI5L1QO/Up7W3oZU3ol3yilE8uW1aJRRe102XqwrD5qfOgOW2\nSuu8bjy+mJ6JdD8JTgOH1wUpZMYJGnkgmiAfePtg8QKBgQD2sjlyGXgvhN1PR7Dx\nGD71tV7H2cARJOdts0LUlt6XWu9+MG72H88JnnbulOTaMj7p0SE4klQHKg+H4ddJ\nHTGxE3CbOXILZvPedPr9zjTHl7Nt0uoSxx2qZQGd5dclZ1EqVP2NY/i9ak2RmF6M\nGQOiWyIhyOTSjcn55IGpig8oUwKBgQDkt8n41C85AViBeCarI+x7Yt/8lpDNHvnm\nYP7eEowBHrvltWskpVIkT9VaFUJnkBwMWPngU+alt7X6KywHMkuFMfQ4FbluBorf\nVt1cIxBDge6yaJbmulM98lAloqAE1LnMbX+WUWIeppVC6lZvozXxGJ120Bsx4hbZ\n68RL3PN06QKBgHKVRWlhuxkXNVPlkKHrCRNHMsbQtQ1BftzNLnVfvfO/IQ4ToPZg\nv+FP4R3fuow+WkpwAGYuUSkiuCP9GemzzajZxqvSes4g+5SP8SSB6rHqHEUeeepO\n4dCLKb0VTGDrJB/tafeKP7Z68Zx2kTOxSqbWfXwDu4SwpueS40bEAVZRAoGBALti\nLDIVvR6qEJX6F0wtYWjxZ+ssfDPhhgIOQ6EzdsrQIEchbEeYvoSLd5OXSVFJywD+\nwLHea2An9Aqi5i57MU5Nz/VHLyWKbKpwI2mCPKfiQuqeL8uWqe5doPZEl299zT9E\nErzXrljISA+LqAdMozwE27Vk6HLk9a3SsH8q5bdJAoGATHKQyMDdb8XK7mJsQ9NZ\n+UlUgTmjci72sxsBEKRqkBMMJOetZUVMCgmKzCq/gd9sTyTwxzzO/Axxadz3Zz6X\nEa94R+rEGEixQoxtzGwIbHJBd1mlJ9K3GQ4PjAmO6vt7s2GlVjuTQmgoKbjIt8Z0\nUbNvv0TTKL9XaI/JDRoCG1g=\n-----END PRIVATE KEY-----\n",
-//   client_email: "firebase-adminsdk-rcogp@dev-ons.iam.gserviceaccount.com",
-//   client_id: "116403685712161920155",
-//   auth_uri: "https://accounts.google.com/o/oauth2/auth",
-//   token_uri: "https://oauth2.googleapis.com/token",
-//   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-//   client_x509_cert_url:
-//     "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-rcogp%40dev-ons.iam.gserviceaccount.com",
-// };
+const serviceAccount = {
+  type: "service_account",
+  project_id: "dev-ons",
+  private_key_id: "7923c4b7ba087475d61b6d7cfbfe2713c4927b05",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDcZ9cyZ3qU/lqj\nBt8QkjI7pyK5Ucl76If5wBMaY+ljD480IVDDILM2EUewmMRASxjv6CvyOSNL80HP\nbXvuRfW5r+yORqEiTS6TRgsoOFN9L0qj9OgVWI0ZrklaYYwZD5+vRAwxvJ0uyK/j\noOwJMRoYozzUY4GXj1pLqIuOUZvpYs41WFucWg5UUSbeubBmvR0mLBcD0Utj6i1S\ne8iDHa3SnyUpayIKeyR8Q6jF+BchSvBISPPgtuJE9P6vGylwqt66AD/Nm+6UVvFO\nvANxwNZjnJdRo5UuL+znYF3qI7ftz9LlWTUd2kOcqEpBB30uGv5OGw1zcwVB/i7P\nRTkg2k+LAgMBAAECggEAKXFxGm4ws4FcQ5lQKkqDHYpg0h3Z1+2yW7jNHsI4qULM\nt42oJPiqGfmH1GH1TO2SA9JRKRurUmWsMzev/O60tLSqs1n+hHek0VZzVJ26vSg+\nTbQIS7M53OFTs6Xx9OhmK0uNS3De0WQ1BoVEvLm54XYYQwtu27O908oea6VWvrxX\nCvquwvqeiLHFMMtr5VE82+WLCGc2nbZsOvIrF6M/2VkENxM3Hcg7vxgLv082P8K5\n8iDHMyBV3Y+ZTNVVI5L1QO/Up7W3oZU3ol3yilE8uW1aJRRe102XqwrD5qfOgOW2\nSuu8bjy+mJ6JdD8JTgOH1wUpZMYJGnkgmiAfePtg8QKBgQD2sjlyGXgvhN1PR7Dx\nGD71tV7H2cARJOdts0LUlt6XWu9+MG72H88JnnbulOTaMj7p0SE4klQHKg+H4ddJ\nHTGxE3CbOXILZvPedPr9zjTHl7Nt0uoSxx2qZQGd5dclZ1EqVP2NY/i9ak2RmF6M\nGQOiWyIhyOTSjcn55IGpig8oUwKBgQDkt8n41C85AViBeCarI+x7Yt/8lpDNHvnm\nYP7eEowBHrvltWskpVIkT9VaFUJnkBwMWPngU+alt7X6KywHMkuFMfQ4FbluBorf\nVt1cIxBDge6yaJbmulM98lAloqAE1LnMbX+WUWIeppVC6lZvozXxGJ120Bsx4hbZ\n68RL3PN06QKBgHKVRWlhuxkXNVPlkKHrCRNHMsbQtQ1BftzNLnVfvfO/IQ4ToPZg\nv+FP4R3fuow+WkpwAGYuUSkiuCP9GemzzajZxqvSes4g+5SP8SSB6rHqHEUeeepO\n4dCLKb0VTGDrJB/tafeKP7Z68Zx2kTOxSqbWfXwDu4SwpueS40bEAVZRAoGBALti\nLDIVvR6qEJX6F0wtYWjxZ+ssfDPhhgIOQ6EzdsrQIEchbEeYvoSLd5OXSVFJywD+\nwLHea2An9Aqi5i57MU5Nz/VHLyWKbKpwI2mCPKfiQuqeL8uWqe5doPZEl299zT9E\nErzXrljISA+LqAdMozwE27Vk6HLk9a3SsH8q5bdJAoGATHKQyMDdb8XK7mJsQ9NZ\n+UlUgTmjci72sxsBEKRqkBMMJOetZUVMCgmKzCq/gd9sTyTwxzzO/Axxadz3Zz6X\nEa94R+rEGEixQoxtzGwIbHJBd1mlJ9K3GQ4PjAmO6vt7s2GlVjuTQmgoKbjIt8Z0\nUbNvv0TTKL9XaI/JDRoCG1g=\n-----END PRIVATE KEY-----\n",
+  client_email: "firebase-adminsdk-rcogp@dev-ons.iam.gserviceaccount.com",
+  client_id: "116403685712161920155",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-rcogp%40dev-ons.iam.gserviceaccount.com",
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
--- utilities/twitter.js
+++ utilities/twitter.js
@@ -28,46 +28,37 @@ const get = (slug) => {
 };
 
 const saveTweets = async (pageToken = "") => {
-  // console.log("lastId", lastId);
   let responseTweets = "";
   if (!lastId) {
     responseTweets = await get(
-      `tweets/search/recent?expansions=author_id,referenced_tweets.id&sort_order=relevancy&tweet.fields=lang,created_at,note_tweet&query=(sport OR calcio) lang:it -is:retweet -is:quote`
+      `tweets/search/recent?expansions=author_id,referenced_tweets.id&tweet.fields=lang,created_at,note_tweet&query=(sport OR calcio) lang:it -is:retweet -is:quote`
     );
   } else if (pageToken) {
     responseTweets = await get(
-      `tweets/search/recent?expansions=author_id,referenced_tweets.id&sort_order=relevancy&tweet.fields=lang,created_at,note_tweet&since_id=${lastId}&query=(sport OR calcio) lang:it -is:retweet -is:quote&pagination_token=${pageToken}`
+      `tweets/search/recent?expansions=author_id,referenced_tweets.id&tweet.fields=lang,created_at,note_tweet&since_id=${lastId}&query=(sport OR calcio) lang:it -is:retweet -is:quote&pagination_token=${pageToken}`
     );
   } else {
     try {
       responseTweets = await get(
-        `tweets/search/recent?expansions=author_id,referenced_tweets.id&sort_order=relevancy&tweet.fields=lang,created_at,note_tweet&since_id=${lastId}&query=(sport OR calcio) lang:it -is:retweet -is:quote`
+        `tweets/search/recent?expansions=author_id,referenced_tweets.id&tweet.fields=lang,created_at,note_tweet&since_id=${lastId}&query=(sport OR calcio) lang:it -is:retweet -is:quote`
       );
     } catch {
       //Se lastId troppo vecchio
       responseTweets = await get(
-        `tweets/search/recent?expansions=author_id,referenced_tweets.id&sort_order=relevancy&tweet.fields=lang,created_at,note_tweet&query=(sport OR calcio) lang:it -is:retweet -is:quote`
+        `tweets/search/recent?expansions=author_id,referenced_tweets.id&tweet.fields=lang,created_at,note_tweet&query=(sport OR calcio) lang:it -is:retweet -is:quote`
       );
     }
   }
 
-  // &since_id=${lastId}
-  // &since_id=${lastId}
-  // query=(sport OR calcio) lang:it -is:retweet -is:quote
-
   const tweets = responseTweets?.data?.data || [];
   const nextPageToken = responseTweets?.data?.meta?.next_token || "";
-  // console.log("tweets", responseTweets.data);
-  console.log("nextPageToken", nextPageToken);
 
   for (t of tweets) {
     count++;
-    console.log(count);
     const lang = t.lang || "";
 
     if (lang === "it") {
       countTweets = await elasticsearch.indexTwit(t, countTweets);
-      console.log("countTweets", countTweets);
     }
 
     if (countTweets >= twitterLength) {
