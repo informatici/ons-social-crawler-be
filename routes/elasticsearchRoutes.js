2b6f24b3290a4c8f7ba177a3176cb27b7ef2a3b5
mwithi <alessandro.domanico@yahoo.it>
2023-11-06 17:32:51 +0100

    Restrict /query endpoint to roles

--- routes/elasticsearchRoutes.js
+++ routes/elasticsearchRoutes.js
@@ -8,7 +8,8 @@ router.get("/info", isAuthorized(["Admin"]), elasticsearchControllers.info);
 router.get("/config", isAuthorized(["Admin"]), elasticsearchControllers.config);
 router.get("/clean", isAuthorized(["Admin"]), elasticsearchControllers.clean);
 
-router.get("/query", 
+router.get("/query",
+  isAuthorized(["Admin", "Teacher", "Trainer"]),
   query("dateFrom").notEmpty(),
   query("dateTo").notEmpty(),
   elasticsearchControllers.search
