ab8704bb3bd54427677b11c53950966b84115bb8
Davide Bertuzzi <bertuzzi@digiunit.it>
2024-01-01 19:52:58 +0100

    TAG: dev-be-202401011852

--- controllers/quizControllers.js
+++ controllers/quizControllers.js
@@ -1,6 +1,6 @@
 const { validationResult } = require("express-validator");
 const elasticsearch = require("../utilities/elasticsearch");
-// const excelJS = require("exceljs");
+const excelJS = require("exceljs");
 
 const create = async (req, res, next) => {
   const errors = validationResult(req);
