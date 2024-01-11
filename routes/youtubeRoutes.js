b22c9eb0e037811961034d762d4c98ed7e102483
Davide Bertuzzi <bertuzzi@digiunit.it>
2024-01-11 14:21:48 +0100

    Commit: date filter

--- routes/youtubeRoutes.js
+++ routes/youtubeRoutes.js
@@ -13,6 +13,8 @@ router.get(
   query("prediction"),
   query("sortLabel"),
   query("sortOrder"),
+  query("dateFrom").notEmpty(),
+  query("dateTo").notEmpty(),
   isAuthorized(["Admin", "Teacher", "Trainer"]),
   youtubeControllers.videosIndexId
 );
@@ -25,6 +27,8 @@ router.get(
   query("prediction"),
   query("sortLabel"),
   query("sortOrder"),
+  query("dateFrom").notEmpty(),
+  query("dateTo").notEmpty(),
   isAuthorized(["Admin", "Teacher", "Trainer"]),
   youtubeControllers.commentsIndex
 );
