18a08acf3f254d7ba4118cc5b410efac7f38a9b6
mwithi <alessandro.domanico@yahoo.it>
2024-03-29 15:45:25 +0100

    Fix score

--- controllers/quizControllers.js
+++ controllers/quizControllers.js
@@ -53,7 +53,7 @@ const create = async (req, res, next) => {
       type: req.body.type,
       description: comment.textDisplay || comment.text,
       hasHate: comment.prediction ? true : false,
-      grade: comment.prediction?.prediction_dict,
+      grade: comment.prediction?.score,
       similarity: comment.prediction?.radiuses_nnr,
       dimensions: comment.prediction?.dimensions,
       tokens: comment.prediction?.tokens,
